1e85da18e8cb2531310ce5a13c71dd8d4105c543
tvercrobbie <robbie.still@oxfordshire.gov.uk>
2020-10-10 18:05:17 +0100

    remove functions not ready yet, edit get_png help

--- R/get_png.R
+++ R/get_png.R
@@ -3,8 +3,15 @@
 #' @keywords png, mapping, ggplot
 #' @export
 #' @examples
-#' logo <- get_png("G:/Logo.png")
-#'
+#' library(sf)
+#' logo <- get_png(system.file("data/TVERC_logo.png", package = "ALERC"))
+#' myshape <- st_read(system.file("data/BerksOxon.shp", package = "ALERC"))
+#' library(ggplot2)
+#' ggplot(myshape) +
+#'   geom_sf() +
+#'   coord_sf(datum = 27700) +
+#'   annotation_custom(logo, xmin = 470000, xmax = 504000, ymin = 240000, ymax = Inf) +
+#'   theme_tverc()
 
 get_png <- function(filename) {
   grid::rasterGrob(png::readPNG(filename), interpolate = TRUE)
